354876c1b5b761e069b2ed4577796e0a8cf7e4df
kylebot0 <kylebot07@gmail.com>
2020-03-13 11:23:24 +0100

    Add :id routing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const routing = require('./routes/posts')
+const routing = require('./routes/routes')
 
 const config = {
     port: 3000
 }
 const app = express();
-const postsRouter = require('./routes/posts');
+const postsRouter = require('./routes/routes');
 
 app.set('view engine', 'ejs')
     .set('views', 'views')
