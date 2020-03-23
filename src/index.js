83bc9ff4c6f1d0d76cbcc432f8f40c5a26aba407
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 12:30:52 +0100

    Add compression

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routing = require('./routes/routes')
-
-const port = process.env.PORT
+const compression = require('compression')
+const port = process.env.PORT || 3000
 const app = express();
 
 app.set('view engine', 'ejs')
     .set('views', 'src/views')
     .use(express.static('src/public'))
-    .use(routing);
+    .use(routing)
+    .use(compression());
 
 app.listen(port, function() {
     console.log(`Application started on port: ${port}`);
