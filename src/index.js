5f63fbc388ea5b05b983d8a7199589bc2d539134
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 12:17:02 +0100

    Fix Port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routing = require('./routes/routes')
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT
 const app = express();
 
 app.set('view engine', 'ejs')
